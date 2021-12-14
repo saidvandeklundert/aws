c528c3712533a89f09cc271dd03e8bc775eff394
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-12-14 21:47:56 +0100

    one

--- SAM/myfirstsam/command.sh
+++ SAM/myfirstsam/command.sh
@@ -3,3 +3,7 @@ aws s3 mb s3://said-my-first-sam
 
 # package cloudformation packate
 aws cloudformation package --s3-bucket said-my-first-sam --template-file template.yaml --output-template-file gen/template-generated.yaml
+
+#deploy
+
+aws cloudformation deploy --template-file gen/template-generated.yaml --stack-name my-first-sam --capabilities CAPABILITY_IAM 
